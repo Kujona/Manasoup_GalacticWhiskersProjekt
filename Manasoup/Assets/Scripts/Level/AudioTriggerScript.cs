fe98816f434933610a31428858bc5aae3188bdd2
Kirito <kilito@pm.me>
2024-03-24 17:11:23 +0100

    only trigger once

--- Manasoup/Assets/Scripts/Level/AudioTriggerScript.cs
+++ Manasoup/Assets/Scripts/Level/AudioTriggerScript.cs
@@ -6,6 +6,8 @@ public class AudioTriggerScript : MonoBehaviour
 {
     public GameObject Player;
     public bool Ist_das_in_der_Vergangenheit;
+    
+    private bool onlyTriggerOnce;
 
     // Start is called before the first frame update
     void Awake()
@@ -15,6 +17,13 @@ public class AudioTriggerScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (onlyTriggerOnce)
+        {
+            return;
+        }
+
+        onlyTriggerOnce = true;
+        
         if (collision.tag == "Player" && Ist_das_in_der_Vergangenheit == true && Player.GetComponent<PlayerActions>().vergangenheit == true)
         { 
             GetComponent<AudioSource>().Play();
